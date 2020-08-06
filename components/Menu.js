e9046e6f06d2891ab5ef5c37dc76390f49e96717
Josh Glantz-Hucks <jahteo@gmail.com>
2020-08-05 21:11:03 -0700

    figured I should save it in a working fashion...

--- components/Menu.js
+++ components/Menu.js
@@ -32,69 +32,71 @@ let menuItems = [
   Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
 */
 
-// const header = document.querySelector(".header")
-// function menuMaker(menuArray) {
-//   // debugger
-//   const menu = document.createElement("div")
-//   menu.classList.add("menu")
-//   const unordList = document.createElement("ul")
-//   menu.appendChild(unordList)
-//   menuArray.map((i) => {
-//     const menuItem = document.createElement("li")
-//     menuItem.textContent = i
-//     unordList.appendChild(menuItem)
-//     return menuItem
-//   })
-//   const menuButton = document.querySelector(".menu-button")
-//   menuButton.addEventListener("click", () => {
-//     menu.classList.toggle("menu--open")
-//   })
-//   return menu
-// }
+const header = document.querySelector(".header")
+function menuMaker(menuArray) {
+  // debugger
+  const menu = document.createElement("div")
+  menu.classList.add("menu")
+  const unordList = document.createElement("ul")
+  menu.appendChild(unordList)
+  menuArray.map((i) => {
+    const menuItem = document.createElement("li")
+    menuItem.textContent = i
+    unordList.appendChild(menuItem)
+    return menuItem
+  })
+  const menuButton = document.querySelector(".menu-button")
+  menuButton.addEventListener("click", () => {
+    menu.classList.toggle("menu--open")
+  })
+  return menu
+}
 
-// const theMenu = menuMaker(menuItems)
-// header.appendChild(theMenu)
+const theMenu = menuMaker(menuItems)
+header.appendChild(theMenu)
 
 
 //blatantly stolen from https://codepen.io/Callum-Martin/pen/eGOzyN for my first animation!!!
 //I understand most of why it works enough to rewrite my js to match, but I did admittely copy/paste directly into CSS & then wrestle the JS to fit bc it's so complicated/pretty.
 //I'm pretty sure i did the class cconverstions, and adjusted menuMaker() correctly. I'm not sure where to put the new code from (declating the variables burgerSidebar - sideText) & the timeline. The .to's in the timeline may also nead they're starting points ('-=1' '-=.7') adjusted to have the second one start at a better time..
 //Most IMPO, I don't think the html is pulling the gsap src correctly. Chrome DevTools is showing a problem with the tl. in haminate()
-const header = document.querySelector(".header")
-
-function menuMaker(menuArray) {
-  // debugger
-  const sidebar = document.createElement("div")
-  sidebar.classList.add("sidebar")
-  const sidemenu = document.createElement("div")
-  sidebar.appendChild(sidemenu)
-  menuArray.map((i) => {
-    const menuItem = document.createElement("li")
-    menuItem.textContent = i
-    sidemenu.appendChild(menuItem)
-    return menuItem
-  })
 
 
+// const header = document.querySelector(".header")
 
-  const menuButton = document.querySelector(".menu-button")
-  menuButton.addEventListener("click", haminate())
-  return sidebar
-}
-console.log(menuMaker(menuItems))
-const theMenu = menuMaker(menuItems)
-header.appendChild(theMenu)
+// function menuMaker(menuArray) {
+//   // debugger
+//   const sidebar = document.createElement("div")
+//   sidebar.classList.add("sidebar")
+//   const sidemenu = document.createElement("div")
+//   sidebar.appendChild(sidemenu)
+//   menuArray.map((i) => {
+//     const menuItem = document.createElement("li")
+//     menuItem.textContent = i
+//     sidemenu.appendChild(menuItem)
+//     return menuItem
+//   })
 
-var burgerSidebar= document.getElementsByClassName("sidebar");
-var sideText= document.getElementsByClassName("sidetext");
 
-tl = new TimelineMax({paused:true, reversed:true});
-tl.timeScale(1);
-tl
-.to(burgerSidebar, 1, {x:550, ease: Power2.easeOut}, '-=1')
-.staggerFrom(sideText, 0.5, {opacity: 0, y: 25, ease: Power1.easeOut}, 0.1, "-=0.7")
-.to(sideText, 0.2, {css:{color:"#000"}});
 
-function haminate() {
- tl.reversed() ? tl.play() : tl.reverse();
-}
+//   const menuButton = document.querySelector(".menu-button")
+//   menuButton.addEventListener("click", haminate())
+//   return sidebar
+// }
+// console.log(menuMaker(menuItems))
+// const theMenu = menuMaker(menuItems)
+// header.appendChild(theMenu)
+
+// var burgerSidebar= document.getElementsByClassName("sidebar");
+// var sideText= document.getElementsByClassName("sidetext");
+
+// tl = new TimelineMax({paused:true, reversed:true});
+// tl.timeScale(1);
+// tl
+// .to(burgerSidebar, 1, {x:550, ease: Power2.easeOut}, '-=1')
+// .staggerFrom(sideText, 0.5, {opacity: 0, y: 25, ease: Power1.easeOut}, 0.1, "-=0.7")
+// .to(sideText, 0.2, {css:{color:"#000"}});
+
+// function haminate() {
+//  tl.reversed() ? tl.play() : tl.reverse();
+// }
